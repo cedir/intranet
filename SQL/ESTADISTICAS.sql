6b5df7df68ea36ea9550f95b58c12b1355f2fe8f
Demian-Moschin <demianmoschin@gmail.com>
2014-03-06 14:47:05 -0300

    se corrige el rango de fechas para control estadistico

--- SQL/ESTADISTICAS.sql
+++ SQL/ESTADISTICAS.sql
@@ -4,7 +4,12 @@ inner join "cedirData"."AlmacenEstudios" as ale on  est."idEstudio" = ale."idEst
 inner join "cedirData"."tblPacientes" as p on  est."idPaciente" = p."id"
 where ale."idEstudio" IN (34,13,49,48)
 
-and est."fechaEstudio" between '2012/05/01' and '2013/06/30' 
+
+--and est."fechaEstudio" between '2010/06/01' and '2011/05/31' 
+--and est."fechaEstudio" between '2011/06/01' and '2012/05/31' 
+--and est."fechaEstudio" between '2012/06/01' and '2013/05/31' 
+-- and est."fechaEstudio" between '2013/06/01' and '2014/05/31' 
+-- and est."fechaEstudio" between '2014/06/01' and '2015/05/31' 
 group by p.sexo
 
 /*
