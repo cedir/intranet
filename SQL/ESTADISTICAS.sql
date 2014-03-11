dc5b138abcc144d1007782a7dab0757970201e4d
Demian-Moschin <demianmoschin@gmail.com>
2014-03-11 16:57:43 -0300

    se corrige el rango de fechas para control estadistico

--- SQL/ESTADISTICAS.sql
+++ SQL/ESTADISTICAS.sql
@@ -2,15 +2,21 @@
 from "cedirData"."tblEstudios" as est  
 inner join "cedirData"."AlmacenEstudios" as ale on  est."idEstudio" = ale."idEstudio" 
 inner join "cedirData"."tblPacientes" as p on  est."idPaciente" = p."id"
-where ale."idEstudio" IN (34,13,49,48)
+where ale."idEstudio" IN (1,18,87,140)
 
 
+and est."fechaEstudio" between '2005/06/01' and '2006/05/31' 
+--and est."fechaEstudio" between '2006/06/01' and '2007/05/31' 
+--and est."fechaEstudio" between '2007/06/01' and '2008/05/31' 
+--and est."fechaEstudio" between '2008/06/01' and '2009/05/31' 
+--and est."fechaEstudio" between '2009/06/01' and '2010/05/31' 
 --and est."fechaEstudio" between '2010/06/01' and '2011/05/31' 
 --and est."fechaEstudio" between '2011/06/01' and '2012/05/31' 
---and est."fechaEstudio" between '2012/06/01' and '2013/05/31' 
--- and est."fechaEstudio" between '2013/06/01' and '2014/05/31' 
+-- and est."fechaEstudio" between '2012/06/01' and '2013/05/31' 
+--and est."fechaEstudio" between '2013/06/01' and '2014/05/31' 
 -- and est."fechaEstudio" between '2014/06/01' and '2015/05/31' 
 group by p.sexo
+
 
 /*
 alto : esofago gastroduodeno video endoscopia (1,18,87,140) idem con polipectomia, idem con toma de biopsia,idem coagulacion con argon plasma
