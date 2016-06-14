e6284e89ce5c25e22bf4ed1092425aee0ee26464
Ignacio Aguilera <jiaguilera@gmail.com>
2016-06-14 19:45:51 -0300

    En el informe de pago a médicos se muestra el importe "Neto" del estudio. Esta cantidad resulta de descontar la parte que retiene Cedir al importe del estudio (importe cobrado menos G.A.s y retenciones externas). Además se muestra en la grilla esta nueva cantidad junto al porcentaje que representa el pago a médico respecto de la misma. Haciendo referencia a esta nueva cantidad, la suma de los porcentajes ACTUANTE + SOLICITANTE es siempre 100%.

--- CedirNegocios/LineaPagoMedico.vb
+++ CedirNegocios/LineaPagoMedico.vb
@@ -1,6 +1,42 @@
 Imports CedirDataAccess
 Imports System.Linq
-Imports Npgsql
+
+Public Class PorcentajeMedico
+    Public Sub New(pMedico As Single, pCedir As Single)
+        Me.Medico = pMedico
+        Me.Cedir = pCedir
+    End Sub
+
+    Private pMedico As Single
+    Public Property Medico() As Single
+        Get
+            Return pMedico
+        End Get
+        Set(ByVal value As Single)
+            pMedico = value
+        End Set
+    End Property
+
+    Private pCedir As Single
+    Public Property Cedir() As Single
+        Get
+            Return pCedir
+        End Get
+        Set(ByVal value As Single)
+            pCedir = value
+        End Set
+    End Property
+
+    Public Function GetEfectivo() As Single
+        If Me.Cedir = 100 Then
+            Return 0
+        Else
+            Return (Me.Medico * 100) / (100 - Me.Cedir)
+        End If
+    End Function
+End Class
+
+
 Public Class LineaPagoMedico
     Private m_estudio As estudio
     Private m_importe As Single
@@ -111,7 +147,7 @@ Public Class LineaPagoMedico
     End Function
 
 
-    Public Function getPorcentaje() As Single
+    Public Function getPorcentaje() As PorcentajeMedico
         'Const ID_OBRA_SOCIAL_GALENO As Integer = 46
         'Const CONSULTA As Integer = 20
         'Const ELECTRO As Integer = 46
@@ -120,16 +156,24 @@ Public Class LineaPagoMedico
 
         Const PORC_ACTUANTE_ECOGRAFIA As Decimal = 70
         Const PORC_SOLICITANTE_ECOGRAFIA As Decimal = 15
+        Const PORC_CEDIR_ECOGRAFIA As Decimal = 15
 
         Const PORC_ACTUANTE_LABORATORIO As Decimal = 70
         Const PORC_SOLICITANTE_LABORATORIO As Decimal = 10
+        Const PORC_CEDIR_LABORATORIO As Decimal = 20
 
         Const PORC_ACTUANTE_LIGADURA_HEMORROIDES As Decimal = 50
+        Const PORC_SOLICITANTE_LIGADURA_HEMORROIDES As Decimal = 0
+        Const PORC_CEDIR_LIGADURA_HEMORROIDES As Decimal = 50
 
         Const PORC_ACTUANTE_PRACTICA_ESPECIAL As Decimal = 75
+        Const PORC_SOLICITANTE_PRACTICA_ESPECIAL As Decimal = 0
+        Const PORC_CEDIR_PRACTICA_ESPECIAL As Decimal = 25
+
 
         Const PORC_ACTUANTE As Decimal = 80
         Const PORC_SOLICITANTE As Decimal = 0
+        Const PORC_CEDIR As Decimal = 20
 
         Const COMB_MED_ACT_BRUNETTI As Integer = 2
         Dim COMB_MED_SOL_AL_50_PORC() As Integer = {578}
@@ -137,29 +181,29 @@ Public Class LineaPagoMedico
 
         If Me.estudio.practica.idEstudio = 20 Then
             'Si el estudio es una consulta, la retencion es del 0%
-            Return PORC_CONSULTA
+            Return New PorcentajeMedico(PORC_CONSULTA, 0)
         End If
 
         Dim EsActuante As Boolean = Me.estudio.getCondicionMedico(idMedico) = "actuante-solicitante" OrElse Me.estudio.getCondicionMedico(idMedico) = "actuante"
 
         'if por tipo de estudio
         If Me.estudio.esEcografia Then
-            Return IIf(EsActuante, PORC_ACTUANTE_ECOGRAFIA, PORC_SOLICITANTE_ECOGRAFIA)
+            Return New PorcentajeMedico(IIf(EsActuante, PORC_ACTUANTE_ECOGRAFIA, PORC_SOLICITANTE_ECOGRAFIA), PORC_CEDIR_ECOGRAFIA)
         ElseIf Me.estudio.esLaboratorio Then
-            Return IIf(EsActuante, PORC_ACTUANTE_LABORATORIO, PORC_SOLICITANTE_LABORATORIO)
+            Return New PorcentajeMedico(IIf(EsActuante, PORC_ACTUANTE_LABORATORIO, PORC_SOLICITANTE_LABORATORIO), PORC_CEDIR_LABORATORIO)
         ElseIf Me.estudio.esLigaduraDeHemorroides() Then
-            Return IIf(EsActuante, PORC_ACTUANTE_LIGADURA_HEMORROIDES, PORC_SOLICITANTE)
+            Return New PorcentajeMedico(IIf(EsActuante, PORC_ACTUANTE_LIGADURA_HEMORROIDES, PORC_SOLICITANTE_LIGADURA_HEMORROIDES), PORC_CEDIR_LIGADURA_HEMORROIDES)
         ElseIf Me.estudio.esPracticaEspecial() Then
-            Return IIf(EsActuante, PORC_ACTUANTE_PRACTICA_ESPECIAL, PORC_SOLICITANTE)
+            Return New PorcentajeMedico(IIf(EsActuante, PORC_ACTUANTE_PRACTICA_ESPECIAL, PORC_SOLICITANTE_PRACTICA_ESPECIAL), PORC_CEDIR_PRACTICA_ESPECIAL)
         Else
             If Me.estudio.medicoActuante.idMedico = COMB_MED_ACT_BRUNETTI Then
                 If COMB_MED_SOL_AL_40_PORC.Any(Function(mid) Me.estudio.medicoSolicitante.idMedico = mid) Then
-                    Return IIf(EsActuante, 0.6 * PORC_ACTUANTE, 0.4 * PORC_ACTUANTE)
+                    Return New PorcentajeMedico(IIf(EsActuante, 0.6 * PORC_ACTUANTE, 0.4 * PORC_ACTUANTE), PORC_CEDIR)
                 ElseIf COMB_MED_SOL_AL_50_PORC.Any(Function(mid) Me.estudio.medicoSolicitante.idMedico = mid) Then
-                    Return IIf(EsActuante, 0.5 * PORC_ACTUANTE, 0.5 * PORC_ACTUANTE)
+                    Return New PorcentajeMedico(IIf(EsActuante, 0.5 * PORC_ACTUANTE, 0.5 * PORC_ACTUANTE), PORC_CEDIR)
                 End If
             End If
-            Return IIf(EsActuante, PORC_ACTUANTE, PORC_SOLICITANTE)
+            Return New PorcentajeMedico(IIf(EsActuante, PORC_ACTUANTE, PORC_SOLICITANTE), PORC_CEDIR)
         End If
     End Function
 
